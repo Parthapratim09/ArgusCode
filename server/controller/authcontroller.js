338c2dcf499f3a9ae08af9929c1508ee5fb309ef
Parthapratim09 <parthapratimmakhal09@gmail.com>
2026-06-03 05:12:21 +0530

    debug

--- server/controller/authcontroller.js
+++ server/controller/authcontroller.js
@@ -75,6 +75,7 @@ export const register = async (req, res) => {
         await newUser.save();
         return res.status(201).json({message:"User registered successfully"});
     }catch(error){
+      console.error("Error during registration:", error);
         return res.status(500).json({message:"Something went wrong"});
     }
 };
