85e09e6bd44f325378fc2e65915996e1f5fe9d3f
Parthapratim09 <parthapratimmakhal09@gmail.com>
2026-06-03 12:08:41 +0530

    mail issue fix

--- server/middleware/emailconfig.js
+++ server/middleware/emailconfig.js
@@ -19,7 +19,7 @@ console.log("BREVO_MAIL:", process.env.BREVO_MAIL);
 
     const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.BREVO_SMTP_USER,
